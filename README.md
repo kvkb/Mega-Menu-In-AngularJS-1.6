af51fb3079777c6ae52c7ebebcf682583d7769ce
bharadwaz.kvk <bharadwaz.kvk@techtreeit.com>
2017-08-22 20:39:54 +0530

    added code & updated Readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Mega-Menu-In-AngularJS-1.6
 Mege Menu directive without JQuery
+
+## Unfinished !! Still In Development Phase.
+
+##Dependencies
+
+- AngularJS 1.6.4
---
+++ mega-menu/js/integrated.js
@@ -0,0 +1,7 @@
+
+//-------------------------- INCLUDE ANGULAR LIBRARIES ---------------------------------------------
+
+document.writeln("<script type='text/javascript' src='js/lib/ang/angular.min.js'></script>");
+document.writeln("<script type='text/javascript' src='js/lib/ang/angular-route.min.js'></script>");
+
+document.writeln("<script type='text/javascript' src='js/app/app.js'></script>");
