213a131ff29295e09da07e3426a892c5262eb800
bharadwaz.kvk <bharadwaz.kvk@techtreeit.com>
2017-09-19 16:52:01 +0530

    changes

--- code/without-jquery/js/integrated.js
+++ code/without-jquery/js/integrated.js
@@ -1,6 +1,4 @@
 
-document.writeln("<script type='text/javascript' src='js/lib/jquery-3.2.1.min.js'></script>");
-
 //-------------------------- INCLUDE ANGULAR LIBRARIES ---------------------------------------------
 
 document.writeln("<script type='text/javascript' src='js/lib/ang/angular.min.js'></script>");
