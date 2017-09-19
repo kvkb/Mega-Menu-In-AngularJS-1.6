f6330be9002f44231ce4e0d44949ccfe63feb3b0
bharadwaz.kvk <bharadwaz.kvk@techtreeit.com>
2017-09-19 16:55:56 +0530

    code segregation

--- code/js/integrated.js
+++
@@ -1,7 +0,0 @@
-
-//-------------------------- INCLUDE ANGULAR LIBRARIES ---------------------------------------------
-
-document.writeln("<script type='text/javascript' src='js/lib/ang/angular.min.js'></script>");
-document.writeln("<script type='text/javascript' src='js/lib/ang/angular-route.min.js'></script>");
-
-document.writeln("<script type='text/javascript' src='js/app/app.js'></script>");
